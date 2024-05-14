6770400a6e6db563ad368aa202d9481c08cc26e1
sainaegorova <egorovasaina00@.gmail.com>
2024-05-14 22:18:00 +0900

    first commit

---
+++ app/src/main/java/com/example/github3/Repos.java
@@ -0,0 +1,18 @@
+package com.example.github3;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Repos {
+
+    @SerializedName("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
